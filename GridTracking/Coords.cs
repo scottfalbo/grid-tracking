6e3243b7988e40181923173cbb7cd9ddadd72658
Scott Falbo <scottfalboart@gmail.com>
2021-10-09 00:14:15 -0700

    Add project files.

---
+++ GridTracking/Coords.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GridTracking
+{
+    public class Coords
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Coords(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ GridTracking/Map.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GridTracking
+{
+    public class Map
+    {
+        public Dictionary<Coords, List<string>> Grid { get; set; }
+
+        public Map(int x, int y)
+        {
+            MakeMap(x, y);
+        }
+
+        private void MakeMap(int x, int y)
+        {
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Grid.Add(new Coords(i, j), new List<string>());
+                }
+            }
+        }
+
+
+    }
+}
